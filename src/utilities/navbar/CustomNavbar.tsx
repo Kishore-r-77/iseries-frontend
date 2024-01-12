2b58ef3ce6bb1b9e83740238417abfa7539ad78e
Kishore-r-77 <kishorework77@gmail.com>
2024-01-12 11:07:04 +0530

    added logo

--- src/utilities/navbar/CustomNavbar.tsx
+++ src/utilities/navbar/CustomNavbar.tsx
@@ -2,13 +2,25 @@ import { IconButton, Tooltip } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
 import styles from "./customNavbar.module.css";
 import { Outlet } from "react-router";
+import CompanyLogo from "../../assets/images/newinsuremologo (1).png";
 
 function CustomNavbar() {
   return (
     <div>
       <header className={styles["nav-header"]}>
         <nav className={styles["nav-list"]}>
-          <h1 id={styles.logo}>Logo</h1>
+          {/* <h1 id={styles.logo}>Logo</h1> */}
+          <span id={styles.logo}>
+            <img
+              src={CompanyLogo}
+              alt="Company Logo - Brief Description"
+              style={{
+                height: "4rem",
+                objectFit: "cover",
+                borderRadius: "2%",
+              }}
+            />
+          </span>
 
           <ul>
             <li>Home</li>
